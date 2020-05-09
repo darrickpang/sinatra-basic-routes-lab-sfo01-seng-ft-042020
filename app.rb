ac97759113f5a45725d7167453b6904bbf339004
aa1b02ab7492d696da6e8ac540ffa187ba623c88e15238d8d026f79bc5d39b3c <darrickpang@DESKTOP-02GI1R6.localdomain>
2020-05-09 16:25:46 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Darrick Pang"
+    end
+
+    get '/hometown' do
+        "My hometown is San Jose"
+    end
+    get '/favorite-song' do
+        "My favorite song is 'I Don't Wanna Miss a Thing'."
+    end
 end
